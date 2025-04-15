6e89feade9d4c12987abd016f51061266ad19316
Charles Aracil <charles.aracil@gmail.com>
2025-04-15 17:30:55 +0200

    solve day 3 year 2015

--- utils/solution_template.py
+++ utils/solution_template.py
@@ -1,11 +1,18 @@
 from typing import Any
 
-from utils.solution import Solution
+from utils.solution import InputParser, Solution
 
 
 class DaySolution(Solution):
+    INPUT_PARSER = InputParser.MULTIPLE_LINES
+
     def parse_data(self, data: Any) -> Any:
+        # you can delete this method if you don't need to parse the data
         return data
+
+    def parse_line(self, line: str) -> str:
+        # you can delete this method if you don't need to parse the line
+        return line
 
     def solve_part1(self, data: Any) -> int:
         # Your part 1 logic here
